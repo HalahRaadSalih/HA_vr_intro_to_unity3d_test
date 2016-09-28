b3200e7fdce64bf3dd05bf5d74902f21c0ea83b9
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-09-28 16:30:01 -0700

    add files for unity project

---
+++ Assets/ExampleBehavScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleBehavScript : MonoBehaviour {
+	int myInt = 10;
+	// Use this for initialization
+	void Start () {
+		Debug.Log (MultiplyByTwo(myInt));
+
+	}
+
+	int MultiplyByTwo(int num){
+		return num * 2;
+	}
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (transform.position.y);
+		if(Input.GetKeyDown(KeyCode.R)){
+			gameObject.GetComponent<Renderer>().material.color = Color.red;
+		}
+		if(Input.GetKeyDown(KeyCode.G)){
+			gameObject.GetComponent<Renderer>().material.color = Color.green;
+		}
+		if(Input.GetKeyDown(KeyCode.B)){
+			gameObject.GetComponent<Renderer>().material.color = Color.blue;
+		}
+	}
+}
